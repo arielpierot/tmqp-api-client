dfb37eb599ae6ffdef77afe693acbfc9dd932a27
Ariel Pierot <ariel.pierot@dcomp.ufs.br>
2019-03-22 02:17:45 +0000

    Alterado código

--- api.go
+++ api.go
@@ -2,21 +2,30 @@ package tmqp
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
+	"github.com/golang/protobuf/proto"
 	"net"
 	"os"
 	"strings"
 )
 
-func Handshake() {
+const (
+	DELIMITER byte = '\n'
+	TYPE      uint = 1
+	SIZE      uint = 4
+	VERSION        = "TMQP 0.1\n"
+)
 
-	conn, err := net.Dial("tcp", ":4000")
+func Handshake() net.Conn {
+
+	conn, err := net.Dial("tcp", ":7788")
 	checkError(err)
-	defer conn.Close()
+	//defer conn.Close()
 
 	handleConn(conn)
 
-	os.Exit(0)
+	return conn
 }
 
 func handleConn(conn net.Conn) {
@@ -32,7 +41,6 @@ func handleConn(conn net.Conn) {
 	}
 
 	checkError(err)
-	conn.Close()
 }
 
 func checkError(err error) {
@@ -40,4 +48,60 @@ func checkError(err error) {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
+}
+
+func SendPackage(conn net.Conn, type_package int8) {
+
+	fmt.Println("TYPE pck: ", type_package)
+
+	type_byte := make([]byte, TYPE)
+	type_byte[0] = (byte(type_package))
+
+	// _, err := conn.Write(type_byte)
+	err := binary.Write(conn, binary.LittleEndian, type_byte)
+
+	if err == nil {
+
+		fmt.Println("ENVIANDO PACOTE...")
+		fmt.Println("TYPE: ", type_byte)
+
+		if type_package == 1 {
+
+			pacote := &QueueDeclare{
+				Name:      "FILA-1",
+				Exclusive: false,
+			}
+
+			data, _ := proto.Marshal(pacote)
+
+			size_package := uint32(len(data))
+			size_bytes := make([]byte, SIZE)
+			binary.LittleEndian.PutUint32(size_bytes, uint32(size_package))
+
+			fmt.Println("SIZE: ", size_bytes)
+
+			err := binary.Write(conn, binary.LittleEndian, size_bytes)
+
+			if err != nil {
+
+				fmt.Println("ERROR - WRITE SIZE: ", err)
+
+			} else {
+
+				fmt.Println(data)
+				_, err := conn.Write([]byte(data))
+
+				if err != nil {
+					fmt.Println("ERROR - WRITE DATA: ", err)
+				} else {
+					fmt.Println("PACOTE ENVIADO...")
+				}
+
+			}
+		}
+
+	} else {
+		fmt.Println("ERROR - WRITE TYPE: ", err)
+	}
+
 }
